731a586772d7d0ee3c38fd79f44327eb0d194481
Thiago Moreira <39631037+thiagomr8@users.noreply.github.com>
2020-03-09 18:21:31 -0300

    inicio do fim

--- _js/script.js
+++ _js/script.js
@@ -49,7 +49,7 @@ function drawChart() {
 
     var options = {
 
-        width: 150,
+        width: 200,
         height: 210,
         bar: {
             groupWidth: "70%"
@@ -58,11 +58,11 @@ function drawChart() {
             position: "none"
         },
         backgroundColor: 'white',
-     };
-    // var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
-    // chart.draw(view, options);
-    // var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values2"));
-    // chart.draw(view, options);
+    };
+    var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+    chart.draw(view, options);
+    var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values2"));
+    chart.draw(view, options);
     var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values3"));
     chart.draw(view, options);
     var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values4"));
@@ -70,10 +70,6 @@ function drawChart() {
     var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values5"));
     chart.draw(view, options);
     var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values6"));
-    chart.draw(view, options);
-    var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values7"));
-    chart.draw(view, options);
-    var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values8"));
     chart.draw(view, options);
 
 }
